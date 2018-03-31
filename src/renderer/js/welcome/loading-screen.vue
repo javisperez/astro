6fa27a541bf48872429c3675fbcae84aaf63d62b
Javis Pérez <javisperez@gmail.com>
2018-03-31 00:23:33 -0400

    Better loading screen

--- src/renderer/js/welcome/loading-screen.vue
+++ src/renderer/js/welcome/loading-screen.vue
@@ -1,12 +1,27 @@
 <script>
 export default {
-    name: 'astro-loading-screen'
+    name: 'astro-loading-screen',
+
+    props: {
+        done: Boolean,
+    },
+
+    mounted() {
+        setTimeout(_ => {
+            this.$refs.progressBar.style.transform = 'scaleX(1)';
+        });
+    }
 }
 </script>
 
 <template>
-<div class="extracting">
-    <fi-loader class="fi-spin"></fi-loader>
-    <p class="mt-2">Working...</p>
+<div class="loading-screen">
+    <p class="mt-2">
+        <div class="logo-bg"></div>
+
+        <div class="progress-bar" :class="{'done': done}">
+            <div class="bar" ref="progressBar"></div>
+        </div>
+    </p>
 </div>
 </template>
