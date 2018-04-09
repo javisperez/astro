1f9ab734ee1626d3b4162cda19d3a0bc3169b9df
Javis Pérez <javisperez@gmail.com>
2018-04-08 21:22:14 -0400

    Size and stroke width on feather-icon component

--- src/renderer/js/support/feather-icons.js
+++ src/renderer/js/support/feather-icons.js
@@ -6,9 +6,21 @@ Object.keys(icons).forEach(icon => {
     components[`fi-${icon}`] = {
         name: `fi-${icon}`,
 
+        props: {
+            // Width and height size
+            size: {
+                default: 24
+            },
+
+            // Stroke width
+            stroke: {
+                default: 2
+            },
+        },
+
         template: `<svg xmlns="http://www.w3.org/2000/svg" class="fi fi-${icon}"
-            width=24 height=24 viewBox="0 0 24 24" fill="none" stroke="currentColor"
-            stroke-width=2 stroke-linecap="round" stroke-linejoin="round">
+            :width="size" :height="size" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+            :stroke-width="stroke" stroke-linecap="round" stroke-linejoin="round">
             ${icons[icon]}
         </svg>`
     };
