4e1cc4718eb833da53254c61bd9db62d630090a5
Javis <javisperez@gmail.com>
2018-04-20 09:40:44 -0400

    Remove unnecesary throw

--- src/renderer/js/extractors/extract.js
+++ src/renderer/js/extractors/extract.js
@@ -3,25 +3,21 @@ import Cbr from './cbr';
 
 export default
 class Extract {
-    getExtractor() {
-        // @todo find a better way to do this
-        const classMap = {
-            cbr: Cbr,
-            cbz: Cbz
-        };
+  getExtractor() {
+    // @todo find a better way to do this
+    const classMap = {
+      cbr: Cbr,
+      cbz: Cbz
+    };
 
-        try {
-            const Extractor = classMap[this.extension];
-            return new Extractor();
-        } catch (e) {
-            throw new Error('Unsupported format');
-        }
-    }
+    const Extractor = classMap[this.extension];
+    return new Extractor();
+  }
 
-    static file(path) {
-        const self = new Extract();
-        self.extension = path.split('.').pop();
+  static file(path) {
+    const self = new Extract();
+    self.extension = path.split('.').pop();
 
-        return self.getExtractor().run(path);
-    }
+    return self.getExtractor().run(path);
+  }
 }
