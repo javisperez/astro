8cf6cec8bd98f74b496bd7b5afbe0449066be675
Javis <javisperez@gmail.com>
2018-04-21 00:21:32 -0400

    Better loading screen

--- src/renderer/js/welcome/loading-screen.vue
+++ src/renderer/js/welcome/loading-screen.vue
@@ -1,27 +1,59 @@
 <script>
-export default {
-    name: 'astro-loading-screen',
+import progressBar from 'support/progress-bar.vue';
+import { setInterval } from 'timers';
 
-    props: {
-        done: Boolean,
+export default {
+  name: 'astro-loading-screen',
+
+  components: {
+    progressBar,
+  },
+
+  props: {
+    done: Boolean,
+    error: Boolean
+  },
+
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+
+  mounted() {
+    this.$progress.start();
+  },
+
+  computed: {
+    $progress() {
+      return this.$refs.progressBar;
+    }
+  },
+
+  watch: {
+    done(value) {
+      if (value) {
+        this.$progress.finish();
+      }
     },
 
-    mounted() {
-        setTimeout(_ => {
-            this.$refs.progressBar.style.transform = 'scaleX(1)';
-        });
+    error(value) {
+      if (value) {
+        this.$progress.finish();
+      }
     }
+  }
 }
 </script>
 
 <template>
-<div class="loading-screen">
-    <p class="mt-2">
-        <div class="logo-bg"></div>
+<div class="loading-screen" :class="{'error': error}">
+  <div class="logo-bg"></div>
 
-        <div class="progress-bar" :class="{'done': done}">
-            <div class="bar" ref="progressBar"></div>
-        </div>
-    </p>
+  <progress-bar ref="progressBar" :duration="10000"></progress-bar>
+
+  <div class="error-message text-white font-lg mt-8" v-if="error">
+    Oh snaps! that doesn't seems like a valid comic file.
+  </div>
 </div>
 </template>
