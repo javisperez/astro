3761deb55ca2913ddf4d153e7dcfd2a478219072
Javis <javisperez@gmail.com>
2018-04-21 00:22:26 -0400

    Spin prop for icons

--- src/renderer/js/support/feather-icons.js
+++ src/renderer/js/support/feather-icons.js
@@ -3,33 +3,42 @@ import icons from 'feather-icons/dist/icons.json';
 const components = {};
 
 Object.keys(icons).forEach(icon => {
-    components[`fi-${icon}`] = {
-        name: `fi-${icon}`,
+  components[`fi-${icon}`] = {
+    name: `fi-${icon}`,
 
-        props: {
-            // Width and height size
-            size: {
-                default: 24
-            },
+    props: {
+      // Width and height size
+      size: {
+        default: 24
+      },
 
-            // Stroke width
-            stroke: {
-                default: 2
-            },
-        },
+      // Stroke width
+      stroke: {
+        default: 2
+      },
 
-        template: `<svg xmlns="http://www.w3.org/2000/svg" class="fi fi-${icon}"
-            :width="size" :height="size" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-            :stroke-width="stroke" stroke-linecap="round" stroke-linejoin="round">
-            ${icons[icon]}
-        </svg>`
-    };
+      spin: {
+        type: Boolean,
+        default: false
+      }
+    },
+
+    template: `
+    <svg xmlns="http://www.w3.org/2000/svg" class="fi fi-${icon}"
+      :class="{'spin': spin}" :width="size" :height="size"
+      viewBox="0 0 24 24" fill="none" stroke="currentColor"
+      :stroke-width="stroke" stroke-linecap="round" stroke-linejoin="round"
+    >
+      ${icons[icon]}
+    </svg>
+    `
+  };
 });
 
 export default {
-    install(Vue) {
-        Vue.mixin({
-            components,
-        });
-    }
+  install(Vue) {
+    Vue.mixin({
+      components,
+    });
+  }
 };
