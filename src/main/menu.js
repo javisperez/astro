c6ebef032e32457c4b435c961af9305938f5091d
Javis <javisperez@gmail.com>
2018-05-10 00:21:00 -0400

    Edit menu

--- src/main/menu.js
+++ src/main/menu.js
@@ -40,6 +40,40 @@ const template = [
     ]
   },
 
+  // Edit
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        role: 'undo'
+      },
+      {
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'cut'
+      },
+      {
+        role: 'copy'
+      },
+      {
+        role: 'paste'
+      },
+      {
+        role: 'pasteandmatchstyle'
+      },
+      {
+        role: 'delete'
+      },
+      {
+        role: 'selectall'
+      }
+    ]
+  },
+
   // Tools
   {
     id: 'tools',
